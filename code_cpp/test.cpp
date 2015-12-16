dbdabf63a39a704e4c396b546618ab6b925d47ca
zhaobaojin1006 <353154585@qq.com>
2015-12-16 19:34:59 +0800

    test

---
+++ code_cpp/test.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: zhaobaojin 
+	> BLOG: blog.csdn.net/zhaobaojin1006 
+	> Mail: 1279742553@qq.com 
+	> Created Time: 2015年11月18日 星期三 21时03分31秒
+ ************************************************************************/
+#include<iostream>
+#include<stdio.h>
+#include<cctype>
+#include<vector>
+using namespace std ;
+using std :: vector ;
+using std :: cin ;
+using std :: cout ;
+using std :: endl ;
+using std :: string ;
+
+int main(void)
+{
+    char a1 , a2, a3 ;
+    scanf("%c %c", &a1, &a2) ;
+    a3= getchar() ;
+    printf("%c, %c, %c", a1, a2, a3) ;
+    return 0 ;
+}
